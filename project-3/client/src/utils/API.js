d890737a26af58a8b86d4f73d04f819ccc018b0b
Will Baird <whbaird21@gmail.com>
2018-11-12 21:13:44 -0600

    Fixed DB

--- project-3/client/src/utils/API.js
+++ project-3/client/src/utils/API.js
@@ -1,20 +1,16 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
   getProducts: function() {
-    return axios.get("/api/books");
+    return axios.get("/api/products");
   },
-  // Gets the book with the given id
   getProduct: function(id) {
-    return axios.get("/api/books/" + id);
+    return axios.get("/api/products/" + id);
   },
-  // Deletes the book with the given id
   deleteProduct: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.delete("/api/products/" + id);
   },
-  // Saves a book to the database
-  saveProduct: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveProduct: function(productData) {
+    return axios.post("/api/products", productData);
   }
 };
--- project-3/routes/api/index.js
+++ project-3/routes/api/index.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const productRoutes = require("./products");
 
-// Book routes
 router.use("/products", productRoutes);
 
 module.exports = router;
